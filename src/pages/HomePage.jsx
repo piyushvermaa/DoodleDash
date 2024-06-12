d7b12770f5bc59d19b46c9b747414484afaa4520
piyushvermaa <piyushsoniiii.07@gmail.com>
2024-06-12 13:41:34 +0530

    responsiveness

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,8 @@ import Avatar from '../components/Avatar';
 const HomePage = () => {
 
   return (
-    <div className='bg-cover bg-center min-h-screen flex flex-col justify-center items-center' style={{ backgroundImage: "url('https://static.vecteezy.com/system/resources/previews/021/736/713/large_2x/doodle-lines-arrows-circles-and-curves-hand-drawn-design-elements-isolated-on-white-background-for-infographic-illustration-vector.jpg')" }}>
-      <h1 className='glow mb-10'>DoodleDash</h1>
+    <div className='bg-cover bg-center min-h-screen flex flex-col justify-center items-center  overflow-hidden' style={{ backgroundImage: "url('https://static.vecteezy.com/system/resources/previews/021/736/713/large_2x/doodle-lines-arrows-circles-and-curves-hand-drawn-design-elements-isolated-on-white-background-for-infographic-illustration-vector.jpg')" }}>
+      <h1 className='glow mb-10 md:text-[6rem] xs:text-[4rem]'>DoodleDash</h1>
       <div className='bg-white bg-opacity-90 p-8 rounded-lg shadow-lg flex flex-col justify-center items-center w-4/5 max-w-md  border border-black'>
         <input className='border border-gray-300 p-2 mb-4 w-full rounded' type="text" placeholder="Enter your username" />
         <Avatar />
